7d39f73d188b7e5111f4c5ffbb871251d5d7486e
Ashwink10 <83913080+Ashwink10@users.noreply.github.com>
2021-07-06 17:21:49 +0530

    Area of circle program

---
+++ areaofcircle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  4 11:15:33 2021
+
+@author: Ashwin
+"""
+PI=3.14
+rad=float(input("Radius of the circle:"))
+area=PI*rad*rad
+print("Area of the circle:%.2f"%area)
